d64fd2892ba156ce364f1721d347f925b88edce7
Eslam Samy <eslamsamymeg@gmail.com>
2019-04-10 23:22:42 +0200

    softmax.py softmax_loss_naive fn second implementation nested loops

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -19,19 +19,49 @@ def softmax_loss_naive(W, X, y, reg):
   - loss as single float
   - gradient with respect to weights W; an array of same shape as W
   """
+#  ## first Implementation
+#  loss = 0.0
+#  dW = np.zeros_like(W)
+#  num_classes = W.shape[1]
+#  num_train = X.shape[0]
+#  #num_Dim = W.shape[0]
+#  
+#  for sampleIndex in range(num_train):
+#      scores = np.dot(X[sampleIndex,],W)
+#      loss += -np.log(np.exp(scores[y[sampleIndex]])/np.exp(scores).sum())
+#  
+#  loss /= num_train  
+#  loss += reg * np.sum(W * W)
+  
+  
+  ### Second Implementations
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
   num_classes = W.shape[1]
   num_train = X.shape[0]
-  #num_Dim = W.shape[0]
+  num_Dim = W.shape[0]
+  scores = []
+  for sampleIndex in range(num_train):#iterate over each train example
+      cscores = []
+      for cIndex in range(num_classes):#iterate over each class in train example
+          cscore = 0.0
+          for wIndex in range(num_Dim):#iterate over each Dim in class weights
+              #print (cscore)
+              cscore += X[sampleIndex, wIndex]*W[wIndex,cIndex]#w*input
+          cscores.append(cscore)#add up the results for each class for each sample
+      scores.append (cscores)
+      
+      
+  scores = np.array(scores)
+  #print(scores.shape)
+  for sampleIndex in range(num_train):#calculate the losses over each sample
+      loss += -np.log(np.exp(scores[sampleIndex,y[sampleIndex]])/np.exp(scores[sampleIndex,]).sum())
   
-  for sampleIndex in range(num_train):
-      scores = np.dot(X[sampleIndex,],W)
-      loss += -np.log(np.exp(scores[y[sampleIndex]])/np.exp(scores).sum())
-  
-  loss /= num_train  
-  loss += reg * np.sum(W * W)
+  loss /= num_train
+  for cIndex in range(num_classes):# add reg to the loss
+      for wIndex in range(num_Dim):
+          loss += reg * W[wIndex, cIndex]
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
